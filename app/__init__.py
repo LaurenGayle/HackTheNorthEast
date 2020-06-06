31289cb8289cf2d02977b8cfe600764ad9c504b9
nicholasblackburn1 <nickblackburn02@gmail.com>
2020-06-06 17:52:26 -0400

    Auto stash before revert of "Live Graph"

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ Copyright (c) 2019 - present AppSeed.us
 import os
 
 from flask            import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 from flask_login      import LoginManager
 from flask_bcrypt     import Bcrypt
 
@@ -16,15 +16,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-#app.config.from_object('app.configuration.Config')
+app.config.from_object('app.configuration.Config')
 
-
+db = SQLAlchemy  (app) # flask-sqlalchemy
 bc = Bcrypt      (app) # flask-bcrypt
 
-#l#m = LoginManager(   ) # flask-loginmanager
-#lm.init_app(app)       # init the login manage
-# Setup database
+lm = LoginManager(   ) # flask-loginmanager
+lm.init_app(app)       # init the login manager
 
+# Setup database
+@app.before_first_request
+def initialize_database():
+    db.create_all()
 
 # Import routing, models and Start the App
-from app import views
+from app import views, models
