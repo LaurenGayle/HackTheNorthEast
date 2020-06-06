cc14e8a9fcbd4da696689f1b6165d87a7ef48f8e
LaurenGayle <40645329+OodleOfNoodlez@users.noreply.github.com>
2020-06-06 19:40:34 -0400

    Fixed Env

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\laure\Desktop\flask-pixel-bootstrap-uikit\venv
+set VIRTUAL_ENV=C:\Users\laure\Desktop\HackTheNorthEast\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
